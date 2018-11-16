4208c437fc087d10fa6ca379882594a89585ce5c
pte <pte@trf4.jus.br>
2018-11-16 14:14:16 -0200

    First Commit

---
+++ ImageCloning.py
@@ -0,0 +1,22 @@
+SRC_IMG = ""
+TRG_IMG = ""
+SRC_MASK = ""
+SYNTH_MASK = ""
+
+def do_em_iter_twosrc_cloning():
+
+
+    return
+
+
+
+def make_blend_sshape(img, gamma):
+
+    return
+
+def __main__():
+
+    print("Maicon")
+
+
+__main__()
